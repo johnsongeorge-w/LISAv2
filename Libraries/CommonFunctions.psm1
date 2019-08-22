689f05357352f8bdc71b77181797985ff8a105cc
Johnson <v-jogeor@microsoft.com>
2019-08-21 19:59:15 -0700

    Custom kernel installation handling of already installed case

--- Libraries/CommonFunctions.psm1
+++ Libraries/CommonFunctions.psm1
@@ -633,6 +633,11 @@ function Install-CustomKernel ($CustomKernel, $allVMData, [switch]$RestartAfterU
 				}
 				return $true
 			} else {
+				$customKernelAlreadyInstall="CUSTOM_KERNEL_ALREADY_INSTALLED"
+				if ($currentStatus -imatch $customKernelAlreadyInstall) {
+					Write-LogInfo "Kernel upgrade skipped in $($jobCount-$kernelSuccess) VMs."
+					return $true
+				}
 				Write-LogErr "Kernel upgrade failed in $($jobCount-$kernelSuccess) VMs."
 				return $false
 			}
